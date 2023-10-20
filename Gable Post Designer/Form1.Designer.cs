bd2d23da79f5ce550e6bfa4c70ecaead80e9a44c
Billy Noble <billy.noble.2022@uni.strath.ac.uk>
2023-10-20 15:39:34 +0100

    Initial Commit.

--- Gable Post Designer/Form1.Designer.cs
+++ Gable Post Designer/Form1.Designer.cs
@@ -36,11 +36,22 @@
             this.windPressure_textBox = new System.Windows.Forms.TextBox();
             this.windSuction_label = new System.Windows.Forms.Label();
             this.windSuction_textBox = new System.Windows.Forms.TextBox();
+            this.imposedLoad_label = new System.Windows.Forms.Label();
+            this.imposedLoad_textBox = new System.Windows.Forms.TextBox();
+            this.serviceLoad_label = new System.Windows.Forms.Label();
+            this.serviceLoad_textBox = new System.Windows.Forms.TextBox();
+            this.deadLoad_label = new System.Windows.Forms.Label();
+            this.deadLoad_textBox = new System.Windows.Forms.TextBox();
+            this.frameCentres_label = new System.Windows.Forms.Label();
+            this.frameCentres_textBox = new System.Windows.Forms.TextBox();
+            this.calculate_button = new System.Windows.Forms.Button();
+            this.trialSection_label = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // postHeight_textBox1
             // 
-            this.postHeight_textBox1.Location = new System.Drawing.Point(107, 59);
+            this.postHeight_textBox1.Location = new System.Drawing.Point(111, 12);
             this.postHeight_textBox1.Name = "postHeight_textBox1";
             this.postHeight_textBox1.Size = new System.Drawing.Size(100, 20);
             this.postHeight_textBox1.TabIndex = 0;
@@ -48,7 +59,7 @@
             // postHeight_label
             // 
             this.postHeight_label.AutoSize = true;
-            this.postHeight_label.Location = new System.Drawing.Point(24, 62);
+            this.postHeight_label.Location = new System.Drawing.Point(28, 15);
             this.postHeight_label.Name = "postHeight_label";
             this.postHeight_label.Size = new System.Drawing.Size(62, 13);
             this.postHeight_label.TabIndex = 1;
@@ -57,7 +68,7 @@
             // postCentres_label
             // 
             this.postCentres_label.AutoSize = true;
-            this.postCentres_label.Location = new System.Drawing.Point(24, 88);
+            this.postCentres_label.Location = new System.Drawing.Point(28, 41);
             this.postCentres_label.Name = "postCentres_label";
             this.postCentres_label.Size = new System.Drawing.Size(67, 13);
             this.postCentres_label.TabIndex = 3;
@@ -65,7 +76,7 @@
             // 
             // postCentres_textBox
             // 
-            this.postCentres_textBox.Location = new System.Drawing.Point(107, 85);
+            this.postCentres_textBox.Location = new System.Drawing.Point(111, 38);
             this.postCentres_textBox.Name = "postCentres_textBox";
             this.postCentres_textBox.Size = new System.Drawing.Size(100, 20);
             this.postCentres_textBox.TabIndex = 2;
@@ -73,7 +84,7 @@
             // windPressure_label
             // 
             this.windPressure_label.AutoSize = true;
-            this.windPressure_label.Location = new System.Drawing.Point(24, 114);
+            this.windPressure_label.Location = new System.Drawing.Point(28, 67);
             this.windPressure_label.Name = "windPressure_label";
             this.windPressure_label.Size = new System.Drawing.Size(76, 13);
             this.windPressure_label.TabIndex = 5;
@@ -81,7 +92,7 @@
             // 
             // windPressure_textBox
             // 
-            this.windPressure_textBox.Location = new System.Drawing.Point(107, 111);
+            this.windPressure_textBox.Location = new System.Drawing.Point(111, 64);
             this.windPressure_textBox.Name = "windPressure_textBox";
             this.windPressure_textBox.Size = new System.Drawing.Size(100, 20);
             this.windPressure_textBox.TabIndex = 4;
@@ -89,7 +100,7 @@
             // windSuction_label
             // 
             this.windSuction_label.AutoSize = true;
-            this.windSuction_label.Location = new System.Drawing.Point(24, 140);
+            this.windSuction_label.Location = new System.Drawing.Point(28, 93);
             this.windSuction_label.Name = "windSuction_label";
             this.windSuction_label.Size = new System.Drawing.Size(71, 13);
             this.windSuction_label.TabIndex = 7;
@@ -97,16 +108,117 @@
             // 
             // windSuction_textBox
             // 
-            this.windSuction_textBox.Location = new System.Drawing.Point(107, 137);
+            this.windSuction_textBox.Location = new System.Drawing.Point(111, 90);
             this.windSuction_textBox.Name = "windSuction_textBox";
             this.windSuction_textBox.Size = new System.Drawing.Size(100, 20);
             this.windSuction_textBox.TabIndex = 6;
+            // 
+            // imposedLoad_label
+            // 
+            this.imposedLoad_label.AutoSize = true;
+            this.imposedLoad_label.Location = new System.Drawing.Point(272, 93);
+            this.imposedLoad_label.Name = "imposedLoad_label";
+            this.imposedLoad_label.Size = new System.Drawing.Size(74, 13);
+            this.imposedLoad_label.TabIndex = 15;
+            this.imposedLoad_label.Text = "Imposed Load";
+            // 
+            // imposedLoad_textBox
+            // 
+            this.imposedLoad_textBox.Location = new System.Drawing.Point(355, 90);
+            this.imposedLoad_textBox.Name = "imposedLoad_textBox";
+            this.imposedLoad_textBox.Size = new System.Drawing.Size(100, 20);
+            this.imposedLoad_textBox.TabIndex = 14;
+            // 
+            // serviceLoad_label
+            // 
+            this.serviceLoad_label.AutoSize = true;
+            this.serviceLoad_label.Location = new System.Drawing.Point(272, 67);
+            this.serviceLoad_label.Name = "serviceLoad_label";
+            this.serviceLoad_label.Size = new System.Drawing.Size(70, 13);
+            this.serviceLoad_label.TabIndex = 13;
+            this.serviceLoad_label.Text = "Service Load";
+            // 
+            // serviceLoad_textBox
+            // 
+            this.serviceLoad_textBox.Location = new System.Drawing.Point(355, 64);
+            this.serviceLoad_textBox.Name = "serviceLoad_textBox";
+            this.serviceLoad_textBox.Size = new System.Drawing.Size(100, 20);
+            this.serviceLoad_textBox.TabIndex = 12;
+            // 
+            // deadLoad_label
+            // 
+            this.deadLoad_label.AutoSize = true;
+            this.deadLoad_label.Location = new System.Drawing.Point(272, 41);
+            this.deadLoad_label.Name = "deadLoad_label";
+            this.deadLoad_label.Size = new System.Drawing.Size(60, 13);
+            this.deadLoad_label.TabIndex = 11;
+            this.deadLoad_label.Text = "Dead Load";
+            // 
+            // deadLoad_textBox
+            // 
+            this.deadLoad_textBox.Location = new System.Drawing.Point(355, 38);
+            this.deadLoad_textBox.Name = "deadLoad_textBox";
+            this.deadLoad_textBox.Size = new System.Drawing.Size(100, 20);
+            this.deadLoad_textBox.TabIndex = 10;
+            // 
+            // frameCentres_label
+            // 
+            this.frameCentres_label.AutoSize = true;
+            this.frameCentres_label.Location = new System.Drawing.Point(272, 15);
+            this.frameCentres_label.Name = "frameCentres_label";
+            this.frameCentres_label.Size = new System.Drawing.Size(75, 13);
+            this.frameCentres_label.TabIndex = 9;
+            this.frameCentres_label.Text = "Frame Centres";
+            // 
+            // frameCentres_textBox
+            // 
+            this.frameCentres_textBox.Location = new System.Drawing.Point(355, 12);
+            this.frameCentres_textBox.Name = "frameCentres_textBox";
+            this.frameCentres_textBox.Size = new System.Drawing.Size(100, 20);
+            this.frameCentres_textBox.TabIndex = 8;
+            // 
+            // calculate_button
+            // 
+            this.calculate_button.Location = new System.Drawing.Point(336, 135);
+            this.calculate_button.Name = "calculate_button";
+            this.calculate_button.Size = new System.Drawing.Size(119, 23);
+            this.calculate_button.TabIndex = 16;
+            this.calculate_button.Text = "Calculate";
+            this.calculate_button.UseVisualStyleBackColor = true;
+            // 
+            // trialSection_label
+            // 
+            this.trialSection_label.AutoSize = true;
+            this.trialSection_label.Location = new System.Drawing.Point(28, 140);
+            this.trialSection_label.Name = "trialSection_label";
+            this.trialSection_label.Size = new System.Drawing.Size(66, 13);
+            this.trialSection_label.TabIndex = 17;
+            this.trialSection_label.Text = "Trial Section";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(166, 21);
+            this.comboBox1.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(488, 179);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.trialSection_label);
+            this.Controls.Add(this.calculate_button);
+            this.Controls.Add(this.imposedLoad_label);
+            this.Controls.Add(this.imposedLoad_textBox);
+            this.Controls.Add(this.serviceLoad_label);
+            this.Controls.Add(this.serviceLoad_textBox);
+            this.Controls.Add(this.deadLoad_label);
+            this.Controls.Add(this.deadLoad_textBox);
+            this.Controls.Add(this.frameCentres_label);
+            this.Controls.Add(this.frameCentres_textBox);
             this.Controls.Add(this.windSuction_label);
             this.Controls.Add(this.windSuction_textBox);
             this.Controls.Add(this.windPressure_label);
@@ -132,6 +244,17 @@
         private System.Windows.Forms.TextBox windPressure_textBox;
         private System.Windows.Forms.Label windSuction_label;
         private System.Windows.Forms.TextBox windSuction_textBox;
+        private System.Windows.Forms.Label imposedLoad_label;
+        private System.Windows.Forms.TextBox imposedLoad_textBox;
+        private System.Windows.Forms.Label serviceLoad_label;
+        private System.Windows.Forms.TextBox serviceLoad_textBox;
+        private System.Windows.Forms.Label deadLoad_label;
+        private System.Windows.Forms.TextBox deadLoad_textBox;
+        private System.Windows.Forms.Label frameCentres_label;
+        private System.Windows.Forms.TextBox frameCentres_textBox;
+        private System.Windows.Forms.Button calculate_button;
+        private System.Windows.Forms.Label trialSection_label;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
